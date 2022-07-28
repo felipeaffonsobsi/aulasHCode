bfb1175d29d287f7ec4015810e740295e9a3c2b2
Felipe Affonso Da Silva Rocha <felipeaffonsobsi@gmail.com>
2022-07-28 00:06:21 -0300

    String de Conexão + Correção
    Stirng de conexão mysql com php + padrão de caracteres utf8

--- pdo/exemplo-02.php
+++ pdo/exemplo-02.php
@@ -1,6 +1,8 @@
 <?php 
 
-$conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+$conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "", array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8" //PARA CARACTERES
+));
 
 $stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES (:LOGIN, :PASSWORD)");
 
