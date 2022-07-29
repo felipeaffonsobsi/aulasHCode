33a92ef482a6878c80affbf23b87ba3622d5ff2d
Felipe Affonso Da Silva Rocha <felipeaffonsobsi@gmail.com>
2022-07-28 23:05:13 -0300

    PDO DAO
    Criando uma classe SQL que extende a classe PDO, onde os comandos de conexão já estão implementados para utilizar na classe SQL

---
+++ DAO/Sql.php
@@ -0,0 +1,59 @@
+<?php
+
+//classe PDO é nativa do PHP
+class Sql extends PDO {
+
+    private $conn;
+
+    //faz a conexão no banco de dados
+    public function __construct() {
+
+        $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+
+    }
+
+    
+    private function setParams($statment, $parameters = array()) {
+
+        foreach ($parameters as $key => $value) {
+
+            $this->setParam($key, $value);
+
+        }
+
+    }
+
+    private function setParam($statment, $key, $value){
+
+        $statment->bindParam($key, $value);
+
+    }
+
+    //primeiro parametro é onde recebe o comando SQL Ex: Insert, Select, Update, Delete...
+    //segundo parametro é os dados que vamos receber
+    public function queryTeste($rawQuery, $params = array()) {
+
+        //variavel stmt não tem this, para funcionar apenas dentro do método
+        //ele está pegando a conn que já está conectada ao banco e preparando o comando sql que passei como parametro
+        $stmt = $this->conn->prepare($rawQuery);
+
+        $this->setParams($stmt, $params);
+
+        $stmt->execute();
+
+        return $stmt;
+
+    }
+
+    public function select($rawQuery, $params = array()):array
+    {
+        
+        $stmt = $this->queryTeste($rawQuery, $params);
+        
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+
+}
+
+?>
