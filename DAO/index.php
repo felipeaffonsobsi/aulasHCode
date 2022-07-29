822b71a7a289dbca625ce57b6be112da9490a8c3
Felipe Affonso Da Silva Rocha <felipeaffonsobsi@gmail.com>
2022-07-29 00:12:36 -0300

    PDO - DAO - LIST
    Criado vários métodos para realizar buscas no banco de dados, carregar usuario, lista de usuarios, usuario autenticado e carregar 1 usuario.

--- DAO/index.php
+++ DAO/index.php
@@ -2,9 +2,22 @@
 
 require_once("config.php");
 
-$root = new Usuario();
-$root->loadById(5);
+//CARREGA 1 USUARIO
+//$root = new Usuario();
+//$root->loadById(5);
+//echo $root;
 
-echo $root;
+//CARREGA UMA LISTA DE USUARIO
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//CARREGA UMA LISTA DE USUARIOS BUSCANDO PELO LOGIN
+//$search = Usuario::search("e");
+//echo json_encode($search);
+
+//CARREGA UM USUARIO USANDO O LOGIN E SENHA
+$usuario = new Usuario();
+$usuario->login("user", "12345");
+echo $usuario;
 
 ?>
