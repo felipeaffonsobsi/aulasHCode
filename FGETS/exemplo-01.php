73eefc3cebd6aae58214bcc6c920a011a47a6934
Felipe Affonso Da Silva Rocha <felipeaffonsobsi@gmail.com>
2022-08-01 11:52:23 -0300

    Inserindo dados em arquivos.
    Inserindo dados em arquivos.

---
+++ FGETS/exemplo-01.php
@@ -0,0 +1,18 @@
+<?php
+
+$filename = "usuarios.csv";
+
+if(file_exists($filename)){
+    $file = fopen($filename, "r");
+    $headers = explode(",", fgets($file));
+    $data = array();
+
+    while($row = fgets($file)){
+       $rowData = explode(", ", $row);
+       
+    }
+
+    fclose($file);
+}
+
+?>
