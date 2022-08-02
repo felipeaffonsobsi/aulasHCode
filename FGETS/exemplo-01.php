34324a933bbdcf1247d262d4bf3337435a2fcecc
Felipe Affonso Da Silva Rocha <felipeaffonsobsi@gmail.com>
2022-08-01 23:52:05 -0300

    Lendo arquivo CSV
    Lendo arquivo csv, linha por linha com while e percorrendo conteudo da linha com for.

--- FGETS/exemplo-01.php
+++ FGETS/exemplo-01.php
@@ -7,12 +7,21 @@ if(file_exists($filename)){
     $headers = explode(",", fgets($file));
     $data = array();
 
-    while($row = fgets($file)){
+    while($row = fgets($file)){ // verifica linha a linha
        $rowData = explode(", ", $row);
-       
+
+       $linha = array();
+       for($i=0; $i < count($headers); $i++){ //verifica conteudo da linha, percorrendo a mesma
+            $linha[$headers[$i]] = $rowData[$i];
+
+       }
+
+       array_push($data, $linha);
     }
 
     fclose($file);
+
+    echo json_encode($data);
 }
 
 ?>
