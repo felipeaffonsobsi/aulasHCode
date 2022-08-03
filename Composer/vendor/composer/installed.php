957bc771014e5ed38be0e253fabd90c503c15e07
Felipe Affonso Da Silva Rocha <felipeaffonsobsi@gmail.com>
2022-08-03 10:07:26 -0300

    Composer
    Instalando o composer

---
+++ Composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hcodebr/composer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3477d026aece33d6e1c57c1cf1afefea6fc09810',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hcodebr/composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3477d026aece33d6e1c57c1cf1afefea6fc09810',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
